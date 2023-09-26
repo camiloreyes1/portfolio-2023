a8f7811a0356ae88300fe1c241e38f063a2b3344
camiloreyes1 <jreye236@fiu.edu>
2023-09-26 19:39:36 -0400

    footer commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
 import './App.css'
 import Nav from './components/Nav'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
-    <>
-    <Nav/>
-    </>
+    <div class="bg-indigo-500">
+
+      <Nav/>
+      <Footer/>
+
+    </div>
+    
   )
 }
 
